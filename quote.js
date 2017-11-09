ccd1a0ba86ee802a2d4302ff67a5bdd67e42d9ac
Chris Lesesne <clesesne3@gmail.com>
2017-11-08 20:40:06 -0800

    add args to User constructor function

---
+++ quote.js
@@ -0,0 +1,30 @@
+'use strict';
+var userInfo = [];
+var projectQuote = [];
+
+function User(fName, lName, company, email, phone) {
+  this.fName = fName;
+  this.lName = lName;
+  this.company = company;
+  this.email = email;
+  this.phone = phone;
+  userInfo.push(this);
+}
+
+function Project() {
+  this.basic = basic;
+  this.eCommerce = eCommerce;
+  this.pages = pages;
+  this.products = products;
+  this.rush = boolean;
+  this.cost = function() {
+    //add base cost
+    //calc pages cost
+    //calc products if eCommerce
+  };
+  this.timeframe = function() {
+    //calc timeFrame
+    //rush order x2
+  };
+  projectQuote.push(this);
+}
