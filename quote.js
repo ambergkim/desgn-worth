ab12df7865b823afdf8a330eae287ff5795c1140
ALICIA LYCAN <alicialycan@gmail.com>
2017-11-08 19:51:57 -0800

    Constructors

---
+++ quote.js
@@ -0,0 +1,22 @@
+'use strict';
+var userInfo = [];
+var projectQuote = [];
+
+function User() {
+  this.fname = fname;
+  this.lname = lname;
+  this.company = comapny;
+  this.email = email;
+  this.phone = phone;
+  userInfo.push(this);
+}
+
+function Project() {
+  this.basic = basic;
+  this.eCommerce = eCommerce;
+  this.pages = pages;
+  this.products = products;
+  this.cost = cost;
+  this.timeframe = timeFrame;
+  projectQuote.push(this);
+}
