95843f92ec4c46c3ee8e4b2aef59762776829506
ALICIA LYCAN <alicialycan@gmail.com>
2017-11-08 21:27:12 -0800

    calc constructor

--- quote.js
+++ quote.js
@@ -11,16 +11,23 @@ function User() {
   userInfo.push(this);
 }
 
-function Project() {
+function Project(basic, eCommerce, pages, products, rush) {
   this.basic = basic;
   this.eCommerce = eCommerce;
-  this.pages = pages;
-  this.products = products;
-  this.rush = boolean;
-  this.cost = function() {
-    //add base cost
-    //calc pages cost
-    //calc products if eCommerce
+  this.pages = parseInt(pages);
+  this.products = parseInt(products);
+  this.rush = rush;
+  this.totalCost = 0;
+  this.calcCost = function() {
+    if (this.basic === true) {
+      this.totalCost += 5000;
+    }
+    if (this.eCommerce === true) {
+      this.totalCost += 15000;
+    }
+    var pagesCost = (Math.floor(this.pages / 5)) * 750;
+    var productsCost = (Math.floor(this.products / 5)) * 750;
+    this.totalCost = this.totalCost + pagesCost + productsCost;
   };
   this.timeframe = function() {
     //calc timeFrame
@@ -28,3 +35,6 @@ function Project() {
   };
   projectQuote.push(this);
 }
+var newProject = new Project(false, true, 12, 10, false);
+newProject.calcCost();
+console.log('For an eCommerce site with 12 pages, 10 products, and no rush, the cost is: $' + newProject.totalCost);
