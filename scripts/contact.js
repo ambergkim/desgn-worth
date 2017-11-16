9f98fac2abb302309fe5c560fb1dbeaaec4698d5
ALICIA LYCAN <alicialycan@gmail.com>
2017-11-15 22:09:27 -0800

    finishingtouches

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,11 +1,12 @@
 'use strict';
 //event listener to grab formButton
 var submitMsg = document.getElementById('submitMsg');
+console.log(submitMsg);
 var formButton = document.getElementById('contactForm');
 var showMsg = function(event) {
   event.preventDefault();
-  var sectionEl = document.getElementById('confirmationMsg');
-  sectionEl.classList.remove('shrunkenConfirm');
-  sectionEl.classList.add('expandedConfirm');
+  var confirmMsg = document.getElementById('confirmMsg');
+  confirmMsg.classList.remove('shrunkenConfirm');
+  confirmMsg.classList.add('expandedConfirm');
 };
 contactForm.addEventListener('submit', showMsg);
